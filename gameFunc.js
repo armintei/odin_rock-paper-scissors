26ea827e6b9aef252ab08aeeb361dba424ee3e82
armintei <tei.min96@gmx.de>
2022-05-05 13:07:57 +0200

    After a long break I finally returned and finished this. There are still some minor flawsbut for now this should do the trick.

--- gameFunc.js
+++ gameFunc.js
@@ -1,45 +1,61 @@
 let playerScore = 0;
 let comScore = 0;
+let round = 0;
 
 function computerPlay() {
     const figures = ['rock', 'paper', 'scissors'];
-    return figures[Math.floor(Math.random() * figures.length)].toUpperCase();
+    return figures[Math.floor(Math.random() * figures.length)];
 }
 
-function playerPlay() {
-    sel = prompt('CHOOSE YOUR WEAPON...')
-    return sel.toUpperCase();
+function playAgain() {
+    window.location.reload();
 }
 
-function game() {
-    for (i = 1; i <= 5; i++) {
-        let comSelection = computerPlay();
-        let playerSelection = playerPlay();
-        if ((playerSelection == 'ROCK' && comSelection == 'SCISSORS') ||
-            (playerSelection == 'PAPER' && comSelection == 'ROCK') ||
-            (playerSelection == 'SCISSORS' && comSelection == 'PAPER')
-        ) {
-            playerScore += 1
-            alert('YOU WIN!');
-        } else if (playerSelection == comSelection) {
-            alert('TIE GAME');
-        } else {
-            comScore += 1;
-            alert('YOU LOSE...');
+function game(btn) {
+    let comFigure = computerPlay([Math.floor]);
+    console.log(comFigure);
+    let plyFigure = btn.id;
+    console.log(plyFigure);
+    if (
+        (plyFigure == 'rock' && comFigure == 'scissors') ||
+        (plyFigure == 'paper' && comFigure == 'rock') ||
+        (plyFigure == 'scissors' && comFigure == 'paper')
+    ) {
+        playerScore++;
+        round++;
+        document.getElementById('result-desc').textContent = 'Good Choice. You beat the Machine for this turn.' 
+        document.getElementById('player-Score').textContent = playerScore
+        document.getElementById('round').textContent = 'Round ' + round
+        document.getElementById('fig-pic').src = comFigure + '.png'
+        if(playerScore == 5)
+        {
+            document.body.classList.add('winScreen')
+            document.getElementById('rock').onclick = null;
+            document.getElementById('paper').onclick = null;
+            document.getElementById('scissors').onclick = null;
+            document.getElementById('result-desc').textContent = 'Congrats! You beat this silicon piece of sh*t.'
+            return;
         }
-        console.log('ROUND: ' + i);
-        console.log(playerSelection + ' ' + comSelection);
-        console.log('YOUR SCORE: ' + playerScore);
-        console.log('COM SCORE: ' + comScore);
-        if (i == 5) {
-            if (comScore < playerScore) {
-                alert('CONGRATULATIONS. YOU HAVE WON WITH ' + playerScore + ' OUT OF 5!');
-            } else if (playerScore == comScore) {
-                alert('IT\'S A TIE GAME!');
-            }
-             else {
-                alert('YOU HAVE LOST WITH ' + playerScore + 'OUT OF 5!');
-            }
+    } else if(plyFigure == comFigure) {
+        round++;
+        document.getElementById('result-desc').textContent = 'Tie Game!'
+        document.getElementById('round').textContent = 'Round ' + round
+        document.getElementById('fig-pic').src = comFigure + '.png'
+    } else {
+        comScore++;
+        round++;
+        document.getElementById('result-desc').textContent = 'Oh no! You lost this round.'
+        document.getElementById('com-Score').textContent = comScore
+        document.getElementById('round').textContent = 'Round ' + round
+        document.getElementById('fig-pic').src = comFigure + '.png'
+        if(comScore == 5)
+        {
+            document.body.classList.add('loseScreen')
+            document.getElementById('rock').onclick = null;
+            document.getElementById('paper').onclick = null;
+            document.getElementById('scissors').onclick = null;
+            document.getElementById('result-desc').textContent = 'The CPU outsmarted you pal...'
+            return;
         }
     }
 }
